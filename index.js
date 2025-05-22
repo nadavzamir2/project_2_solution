599651c24404f4ddc49508c54e9282ade1611262
nadavzamir2 <nadavzamir2@gmail.com>
2025-05-22 21:49:50 +0300

    hii

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-function hello () {
-    console.log("say hello to nadav")
+async function getCoins() {
+    const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd"
+    const json = await fetch(url).then(result => result.json()).catch((err) => {
+        console.error(err);
+        return [];
+    });
+
+
+    console.log(json);
+    return json;
 }
+
+function renderCards(coins) {
+    const cards = coins.map((coin) => {
+        return `<div> ${coin.id}</div>`;
+    });
+    document.getElementById("cards").innerHTML = cards.join('')
+}
+
+async function init() {
+    const coins = await getCoins();
+    renderCards(coins);
+}
+init();
